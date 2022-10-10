cb851847ca06accca03f0c01751009de3e129b8d
Rita Ferreira <reflexo.oxelfer@gmail.com>
2022-10-10 15:14:05 +0100

    Arrays with message components

--- messageCreator.js
+++ messageCreator.js
@@ -1,2 +1,30 @@
 // Codecademy Project - message generator
 // this will be the solution file
+
+const subjectMessage = [
+  "The administrator",
+  "Everyone",
+  "Peter Telleborian",
+  "We",
+  "My brother",
+  "I",
+  "Robot Unit-010",
+];
+const predicateMessage = [
+  "managed",
+  "agreed",
+  "loved",
+  "decided",
+  "enjoyed",
+  "began",
+  "desired",
+];
+const complementMessage = [
+  "to endebt several estates.",
+  "to develop new strategies.",
+  "to eat carrots.",
+  "to have nice things.",
+  "to have long walks.",
+  "to itch all over.",
+  "to rule the universe.",
+];
